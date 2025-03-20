3ea743f87439d54c6979818ef87bd8a4a6afe02f
unknown <debbiemeer30@gmail.com>
2025-03-20 21:58:51 +0800

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.tsx';
 import Hero from './components/Hero';
 import ProblemSolution from './components/ProblemSolution.tsx';
@@ -9,21 +10,32 @@ import AISection from './components/AISection';
 import ScamAlerts from './components/ScamAlerts';
 import Footer from './components/Footer.tsx';
 import ChatButton from './components/ChatButton';
+import SubmitProject from './components/SubmitProject';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-sentinel-dark-900 to-sentinel-dark-800 text-sentinel-white">
-      <Navbar />
-      <Hero />
-      <ProblemSolution />
-      <Features />
-      <TrustScore />
-      <CommunitySection />
-      <AISection />
-      <ScamAlerts />
-      <Footer />
-      <ChatButton />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="min-h-screen bg-gradient-to-b from-sentinel-dark-900 to-sentinel-dark-800 text-sentinel-white">
+              <Navbar />
+              <Hero />
+              <ProblemSolution />
+              <Features />
+              <TrustScore />
+              <CommunitySection />
+              <AISection />
+              <ScamAlerts />
+              <Footer />
+              <ChatButton />
+            </div>
+          }
+        />
+        <Route path="/submit" element={<SubmitProject />} />
+      </Routes>
+    </Router>
   );
 }
 
