30b6b076ea8c8ef47f4de181ed099f613ea52e18
unknown <debbiemeer30@gmail.com>
2025-03-23 00:52:31 +0800

    updated Watchlist page

--- src/components/watchlist/Watchlist.tsx
+++ src/components/watchlist/Watchlist.tsx
@@ -9,15 +9,15 @@ import ProtectionFeatures from './ProtectionFeatures';
 export default function Watchlist() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-sentinel-dark-900 to-sentinel-dark-800">
-      <Navbar />
-      
-      <div className="pt-20 pb-12">
-        <WatchlistHeader />
-        <WatchlistStats />
-        <WatchlistEntries />
-        <ProtectionFeatures />
+      <div className="relative">
+        <Navbar />
+        <div className="pt-32 pb-12">
+          <WatchlistHeader />
+          <WatchlistStats />
+          <WatchlistEntries />
+          <ProtectionFeatures />
+        </div>
       </div>
-
       <Footer />
     </div>
   );
