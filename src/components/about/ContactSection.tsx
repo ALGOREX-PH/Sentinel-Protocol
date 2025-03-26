582f593a9afec713318dc7d2cb7e60c41956bdb9
unknown <debbiemeer30@gmail.com>
2025-03-26 21:59:26 +0800

    updated ContactSection COmponent

--- src/components/about/ContactSection.tsx
+++ src/components/about/ContactSection.tsx
@@ -3,7 +3,7 @@ import { Mail, MessageCircle, Twitter } from 'lucide-react';
 
 export default function ContactSection() {
   return (
-    <div className="py-24 bg-blue-600">
+    <div className="py-24 bg-sentinel-dark-800/50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
           <div>
